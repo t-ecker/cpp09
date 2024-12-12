8627c83671334724e83ec5933da54e4ea1768834
t-ecker <152566019+t-ecker@users.noreply.github.com>
2024-12-12 10:55:36 +0100

    Added files for ex00

---
+++ ex00/BitcoinExchange.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tomecker <tomecker@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/29 14:41:14 by tecker            #+#    #+#             */
+/*   Updated: 2024/12/11 19:27:41 by tomecker         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BITCOINEXCHANGE_HPP
+#define BITCOINEXCHANGE_HPP
+
+#include <string.h>
+#include <iostream>
+#include <iomanip>
+
+
+
+#endif
