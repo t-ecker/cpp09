b5bfd2654c049934c431cc4e4fb7e6e343d1fa66
t-ecker <152566019+t-ecker@users.noreply.github.com>
2024-12-13 15:23:24 +0100

    added constructors for ex00

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -3,11 +3,44 @@
 /*                                                        :::      ::::::::   */
 /*   BitcoinExchange.cpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tomecker <tomecker@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tecker <tecker@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 14:41:10 by tecker            #+#    #+#             */
-/*   Updated: 2024/12/11 19:26:52 by tomecker         ###   ########.fr       */
+/*   Updated: 2024/12/13 14:33:04 by tecker           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
+
+void RPN::calculate(void)
+{
+    
+}
+
+RPN::RPN()
+    :   _stack()
+{
+}
+RPN::RPN(std::string str)
+    :   _str(str), _stack()
+{
+    
+}
+RPN::~RPN()
+{
+}
+RPN::RPN(const RPN &src)
+    : _stack()
+{
+    _str = src._str;
+    _stack = src._stack;
+}
+RPN &RPN::operator=(const RPN &src)
+{
+    if (this != &src)
+    {
+        _str = src._str;
+        _stack = src._stack;
+    }
+    return (*this);
+}
--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tomecker <tomecker@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tecker <tecker@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 14:41:14 by tecker            #+#    #+#             */
-/*   Updated: 2024/12/11 19:27:41 by tomecker         ###   ########.fr       */
+/*   Updated: 2024/12/13 14:32:27 by tecker           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,20 @@
 #include <string.h>
 #include <iostream>
 #include <iomanip>
+#include <map>
 
+class BCE
+{
+    private:
+        std::map<std::string, double> data;
+    public:
+        BCE();
+        BCE(std::string str);
+        ~BCE();
+        BCE(const BCE &src);
+        BCE &operator=(const BCE &src);
+        void calculate(void);
+};
 
 
 #endif
